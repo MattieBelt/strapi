45675e8340cfbff54f929cef84117fea7aab13b3
Mattias van den Belt <mattiasvandenbelt@gmail.com>
2024-06-21 12:39:06 +0200

    feat: update build.js script

--- scripts/build.js
+++ scripts/build.js
@@ -1,32 +1,36 @@
 const { build: buildPackUp, utils } = require('@strapi/pack-up');
 const { build: buildVite, loadConfigFromFile, mergeConfig } = require('vite');
 const { existsSync } = require('fs');
+const { Sign } = require('crypto');
 
 const VITE_CONFIG_PATH = 'vite.config.mjs';
+const DEBUG = process.env.DEBUG === 'true';
+const SILENT = process.env.SILENT === 'true';
 
 const dir = process.cwd();
 const project = process.env.NX_PROJECT_ROOT_PATH;
-const logger = utils.createLogger({ debug: false, silent: true });
+const logger = utils.createLogger({ debug: DEBUG, silent: SILENT });
 
 console.log('Building', project);
 
 const updatePackUpConfig = (config) => {
+  if (config.bundles) {
+    config.bundles = config.bundles.map((bundle) => {
+      // Strapi server only needs CommonJs
+      if (bundle.runtime === 'node') {
+        delete bundle.import;
+      }
+      // Strapi admin panel only needs ESM
+      if (bundle.runtime === 'web') {
+        delete bundle.require;
+      }
+
+      return bundle;
+    });
+  }
+
   return {
     ...config,
-    bundles: config.bundles
-      ? config.bundles.map((bundle) => {
-          // Strapi server only needs CommonJs
-          if (bundle.runtime === 'node') {
-            delete bundle.import;
-          }
-          // Strapi admin panel only needs ESM
-          if (bundle.runtime === 'web') {
-            delete bundle.require;
-          }
-
-          return bundle;
-        })
-      : undefined,
     minify: false,
     preserveModules: true,
     preserveModulesRoot: project,
@@ -61,21 +65,21 @@ const build = async () => {
 
   if (packUpConfig) {
     await buildPackUp({
-      silent: true,
+      silent: SILENT,
       cwd: dir,
       configFile: false,
       config: updatePackUpConfig(packUpConfig),
     });
   }
 
-  if (existsSync(dir + VITE_CONFIG_PATH)) {
-    const { config: viteConfig } = await loadConfigFromFile({}, dir + VITE_CONFIG_PATH).catch(
+  if (existsSync(dir + '/' + VITE_CONFIG_PATH)) {
+    const { config: viteConfig } = await loadConfigFromFile({}, dir + '/' + VITE_CONFIG_PATH).catch(
       () => ({})
     );
 
     if (viteConfig) {
       await buildVite({
-        silent: true,
+        silent: SILENT,
         configFile: false,
         root: dir,
         ...updateViteConfig(viteConfig),
